cedc6adaaebc08ddf3ba4b0edcfed387400ce5aa
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2016-11-08 17:05:12 +0000

    Fix test

--- classifiers/hoeffding/tree_test.go
+++ classifiers/hoeffding/tree_test.go
@@ -21,7 +21,7 @@ var _ = Describe("Tree", func() {
 
 			model := testdata.BigClassificationModel()
 			stats := eval.NewClassification(model)
-			info, err := runBigDataTest(model, stats, n, "../../testdata/bigcls.csv")
+			info, err := runBigDataTest(model, stats, n, "../../testdata/bigcls.csv", nil)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(info).To(Equal(expInfo))
 			Expect(stats.Correct() * 100).To(BeNumerically("~", expCorrect, 0.1))
@@ -55,7 +55,7 @@ var _ = Describe("Tree", func() {
 
 			model := testdata.BigRegressionModel()
 			stats := eval.NewRegression(model)
-			info, err := runBigDataTest(model, stats, n, "../../testdata/bigreg.csv")
+			info, err := runBigDataTest(model, stats, n, "../../testdata/bigreg.csv", nil)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(info).To(Equal(expInfo))
 			Expect(stats.R2()).To(BeNumerically("~", expR2, 0.01))
@@ -67,20 +67,22 @@ var _ = Describe("Tree", func() {
 			NumActiveLeaves: 602,
 			MaxDepth:        2,
 		}, 0.13, 0.80),
-
-		Entry("10,000", 10000, &TreeInfo{
-			NumNodes:        4736,
-			NumActiveLeaves: 4732,
-			MaxDepth:        3,
-		}, 0.21, 0.89),
-
-		Entry("20,000", 20000, &TreeInfo{
-			NumNodes:        8733,
-			NumActiveLeaves: 8725,
-			MaxDepth:        3,
-		}, 0.31, 0.42),
 	)
 
+	It("should prune", func() {
+		model := testdata.BigRegressionModel()
+		stats := eval.NewRegression(model)
+		info, err := runBigDataTest(model, stats, 5000, "../../testdata/bigreg.csv", &Config{
+			HeapTarget: 2 * 1024 * 1024,
+		})
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(info.NumInactiveLeaves).To(BeNumerically("~", 2100, 100))
+		Expect(info.NumNodes).To(BeNumerically("~", 2500, 100))
+		Expect(info.NumActiveLeaves).To(BeNumerically("~", 400, 100))
+		Expect(stats.R2()).To(BeNumerically("~", 0.17, 0.01))
+		Expect(stats.RMSE()).To(BeNumerically("~", 0.97, 0.01))
+	})
 })
 
 func TestSuite(t *testing.T) {
@@ -88,7 +90,7 @@ func TestSuite(t *testing.T) {
 	RunSpecs(t, "classifiers/hoeffding")
 }
 
-func runBigDataTest(model *core.Model, stats eval.Evaluator, n int, fname string) (*TreeInfo, error) {
+func runBigDataTest(model *core.Model, stats eval.Evaluator, n int, fname string, config *Config) (*TreeInfo, error) {
 	stream, err := testdata.Open(fname, model)
 	if err != nil {
 		return nil, err
@@ -100,7 +102,7 @@ func runBigDataTest(model *core.Model, stats eval.Evaluator, n int, fname string
 		return nil, err
 	}
 
-	tree := New(model, nil)
+	tree := New(model, config)
 	for _, inst := range insts[:n] {
 		tree.Train(inst)
 	}
