12023bd83927ae97e22264ce6e9c4c018af3c7f0
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2018-05-18 11:55:15 +0100

    Consistent style

--- java/src/com/blacksquaremedia/reason/classification/FTRL.java
+++ java/src/com/blacksquaremedia/reason/classification/FTRL.java
@@ -11,44 +11,44 @@ import net.jpountz.xxhash.XXHash64;
 import net.jpountz.xxhash.XXHashFactory;
 
 public class FTRL {
-	private FTRLProtos.Optimizer optimizer;
-	private XXHashFactory hasher;
-	private CoreProtos.Model model;
-	private ArrayList<String> predictors;
-	private int[] offsets;
-	private double alpha;
-	private double beta;
-	private double l1;
-	private double l2;
+  private FTRLProtos.Optimizer optimizer;
+  private XXHashFactory hasher;
+  private CoreProtos.Model model;
+  private ArrayList<String> predictors;
+  private int[] offsets;
+  private double alpha;
+  private double beta;
+  private double l1;
+  private double l2;
 
-	public FTRL(FTRLProtos.Optimizer optimizer) {
-		this(optimizer, 0.1, 1.0, 1.0, 0.1);
-	}
+  public FTRL(FTRLProtos.Optimizer optimizer) {
+    this(optimizer, 0.1, 1.0, 1.0, 0.1);
+  }
 
-	public FTRL(FTRLProtos.Optimizer optimizer, double alpha, double beta, double l1, double l2) {
-		this.optimizer = optimizer;
-		this.hasher = XXHashFactory.fastestInstance();
-		this.model = this.optimizer.getModel();
-		this.predictors = new ArrayList<String>(model.getFeaturesCount()-1);
-		this.offsets = new int[model.getFeaturesCount()-1];
-		this.alpha = alpha;
-		this.beta = beta;
-		this.l1 = l1;
-		this.l2 = l2;
+  public FTRL(FTRLProtos.Optimizer optimizer, double alpha, double beta, double l1, double l2) {
+    this.optimizer = optimizer;
+    this.hasher = XXHashFactory.fastestInstance();
+    this.model = this.optimizer.getModel();
+    this.predictors = new ArrayList<String>(model.getFeaturesCount()-1);
+    this.offsets = new int[model.getFeaturesCount()-1];
+    this.alpha = alpha;
+    this.beta = beta;
+    this.l1 = l1;
+    this.l2 = l2;
 
-		// Build a sorted list of feature names
-		String targetName = optimizer.getTarget();
-		for (String name : this.model.getFeaturesMap().keySet()) {
-			if (name != targetName) {
-				this.predictors.add(name);
-			}
-		}
-		Collections.sort(this.predictors);
+    // Build a sorted list of feature names
+    String targetName = optimizer.getTarget();
+    for (String name : this.model.getFeaturesMap().keySet()) {
+      if (targetName.compareTo(name) != 0) {
+        this.predictors.add(name);
+      }
+    }
+    Collections.sort(this.predictors);
 
-		// Pre-calculate predictor offsets
-		int pos = 0;
-		for (int i = 0; i < this.predictors.size(); i++) {
-			this.offsets[i] = pos;
+    // Pre-calculate predictor offsets
+    int pos = 0;
+    for (int i = 0; i < this.predictors.size(); i++) {
+      this.offsets[i] = pos;
 
       CoreProtos.Feature feature = this.model.getFeaturesOrThrow(this.predictors.get(i));
       switch (feature.getKind()) {
@@ -59,23 +59,24 @@ public class FTRL {
         pos += 1;
         break;
       }
-		}
-	}
+    }
+  }
 
-	// Predict returns a single probability for this example.
-	public double predict(Example example) {
-		double wTx = 0.0;
+  // Predict returns a single probability for this example.
+  public double predict(Example example) {
+    double wTx = 0.0;
 
-		// Iterate over sorted predictors.
-		for (int i = 0; i < this.predictors.size(); i++) {
-			wTx += this.increment(example, this.predictors.get(i), this.offsets[i]);
-		}
+    // Iterate over sorted predictors.
+    for (int i = 0; i < this.predictors.size(); i++) {
+      wTx += this.increment(example, this.predictors.get(i), this.offsets[i]);
+    }
 
-		// Apply sigmoid function to calculate probability.
-		return 1.0 / (1.0 + Math.exp(-Math.max(Math.min(wTx, 35), -35)));
-	}
+    // Apply sigmoid function to calculate probability.
+    return 1.0 / (1.0 + Math.exp(-Math.max(Math.min(wTx, 35), -35)));
+  }
 
-	private double increment(Example x, String predictor, int offset) {
+  // Returns a weight increment for every
+  private double increment(Example x, String predictor, int offset) {
     CoreProtos.Feature feature = this.model.getFeaturesOrThrow(predictor);
 
     // Get the example value for the predictor
@@ -84,38 +85,43 @@ public class FTRL {
       return 0.0;
     }
 
-    // Default bucket offset and value
-    int bucket = offset;
-    double value = 1.0;
-
     // Adjust bucket offset and value
+    double value = Double.NaN;
+
     switch (feature.getKind()) {
       case CATEGORICAL:
-      	bucket += this.categoricalOffset(feature, obj.toString());
+        int index = this.category(feature, obj.toString());
+        if (index > -1) { // category found
+          offset += index;
+          value = 1.0;
+        }
         break;
       case NUMERICAL:
-		    if (obj instanceof Number) {
-		      value = ((Number) obj).doubleValue();
-		    }
+        if (obj instanceof Number) {
+          value = ((Number) obj).doubleValue();
+        }
         break;
-      default:
-      	return 0.0;
     }
 
-		double weight = this.optimizer.getWeights(bucket);
-		double sign = weight < 0 ? -1.0 : 1.0;
-		double abs = weight * sign;
-		if (abs <= this.l1) {
-			return 0.0;
-		}
+    // Ensure our value is a number.
+    if (value == Double.NaN) {
+      return 0.0;
+    }
 
-		double sum = Math.sqrt(this.optimizer.getSums(bucket));
-		double step = this.l2 + (this.beta+sum)/this.alpha;
-		return sign * (this.l1 - abs) / step * value;
-	}
+    double weight = this.optimizer.getWeights(offset);
+    double sign = weight < 0 ? -1.0 : 1.0;
+    double abs = weight * sign;
+    if (abs <= this.l1) {
+      return 0.0;
+    }
+
+    double sum = Math.sqrt(this.optimizer.getSums(offset));
+    double step = this.l2 + (this.beta+sum)/this.alpha;
+    return sign * (this.l1 - abs) / step * value;
+  }
 
 
-	private int categoricalOffset(CoreProtos.Feature feature, String value) {
+  private int category(CoreProtos.Feature feature, String value) {
     ProtocolStringList vocabulary = feature.getVocabularyList();
 
     // Check if that string is included in the vocabulary; use the index if so.
@@ -133,5 +139,5 @@ public class FTRL {
     BigInteger bucket = bigInt.remainder(BigInteger.valueOf(numBuckets));
 
     return bucket.intValue() + vocabulary.size();
-	}
+  }
 }
--- java/src/com/blacksquaremedia/reason/core/MapExample.java
+++ java/src/com/blacksquaremedia/reason/core/MapExample.java
@@ -3,14 +3,13 @@ package com.blacksquaremedia.reason.core;
 import java.util.Map;
 
 public class MapExample implements Example {
-	private java.util.Map<String,Object> map;
+  private java.util.Map<String,Object> map;
 
-	MapExample(java.util.Map<String,Object> map)	{
-		this.map = map;
-	}
+  MapExample(java.util.Map<String,Object> map)    {
+    this.map = map;
+  }
 
-	public Object getExampleValue(java.lang.String featureName) {
-		return this.map.get(featureName);
-	}
-
+  public Object getExampleValue(java.lang.String featureName) {
+    return this.map.get(featureName);
+  }
 }
