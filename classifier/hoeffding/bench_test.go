aa2c08ab832db8b47b9c875cc66f0335b3c94815
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2018-11-05 17:04:54 +0000

    Adding examples + benchmark

---
+++ classifier/hoeffding/bench_test.go
@@ -0,0 +1,112 @@
+package hoeffding_test
+
+import (
+	"testing"
+
+	"github.com/bsm/reason/classifier/hoeffding"
+	"github.com/bsm/reason/testdata"
+)
+
+func BenchmarkTree_Train_classification(b *testing.B) {
+	const N = 1000
+
+	stream, model, err := testdata.OpenBigData("classification", "../../testdata")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer stream.Close()
+
+	examples, err := stream.ReadN(N)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	tree, err := hoeffding.New(model, "target", nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		tree.Train(examples[i%N])
+	}
+}
+
+func BenchmarkTree_Train_classification_parallel(b *testing.B) {
+	const N = 1000
+
+	stream, model, err := testdata.OpenBigData("classification", "../../testdata")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer stream.Close()
+
+	examples, err := stream.ReadN(N)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	tree, err := hoeffding.New(model, "target", nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			i++
+			tree.Train(examples[i%N])
+		}
+	})
+}
+
+func BenchmarkTree_Train_regression(b *testing.B) {
+	const N = 1000
+
+	stream, model, err := testdata.OpenBigData("regression", "../../testdata")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer stream.Close()
+
+	examples, err := stream.ReadN(N)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	tree, err := hoeffding.New(model, "target", nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		tree.Train(examples[i%N])
+	}
+}
+
+func BenchmarkTree_Train_regression_parallel(b *testing.B) {
+	const N = 1000
+
+	stream, model, err := testdata.OpenBigData("regression", "../../testdata")
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer stream.Close()
+
+	examples, err := stream.ReadN(N)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	tree, err := hoeffding.New(model, "target", nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			i++
+			tree.Train(examples[i%N])
+		}
+	})
+}
---
+++ classifier/hoeffding/example_test.go
@@ -0,0 +1,120 @@
+package hoeffding_test
+
+import (
+	"fmt"
+
+	"github.com/bsm/reason/classifier/hoeffding"
+	"github.com/bsm/reason/core"
+)
+
+func Example_classification() {
+	model := core.NewModel(
+		core.NewCategoricalFeature("play", []string{"yes", "no"}),
+		core.NewCategoricalFeature("outlook", []string{"rainy", "overcast", "sunny"}),
+		core.NewCategoricalFeature("temp", []string{"hot", "mild", "cool"}),
+		core.NewCategoricalFeature("humidity", []string{"normal", "high"}),
+		core.NewCategoricalFeature("windy", []string{"true", "false"}),
+	)
+
+	examples := []core.MapExample{
+		{"outlook": "rainy", "temp": "hot", "humidity": "high", "windy": "false", "play": "no"},
+		{"outlook": "rainy", "temp": "hot", "humidity": "high", "windy": "true", "play": "no"},
+		{"outlook": "overcast", "temp": "hot", "humidity": "high", "windy": "false", "play": "yes"},
+		{"outlook": "sunny", "temp": "mild", "humidity": "high", "windy": "false", "play": "yes"},
+		{"outlook": "sunny", "temp": "cool", "humidity": "normal", "windy": "false", "play": "yes"},
+		{"outlook": "sunny", "temp": "cool", "humidity": "normal", "windy": "true", "play": "no"},
+		{"outlook": "overcast", "temp": "cool", "humidity": "normal", "windy": "true", "play": "yes"},
+		{"outlook": "rainy", "temp": "mild", "humidity": "high", "windy": "false", "play": "no"},
+		{"outlook": "rainy", "temp": "cool", "humidity": "normal", "windy": "false", "play": "yes"},
+		{"outlook": "sunny", "temp": "mild", "humidity": "normal", "windy": "false", "play": "yes"},
+		{"outlook": "rainy", "temp": "mild", "humidity": "normal", "windy": "true", "play": "yes"},
+		{"outlook": "overcast", "temp": "mild", "humidity": "high", "windy": "true", "play": "yes"},
+		{"outlook": "overcast", "temp": "hot", "humidity": "normal", "windy": "false", "play": "yes"},
+		{"outlook": "sunny", "temp": "mild", "humidity": "high", "windy": "true", "play": "no"},
+	}
+
+	// Init with a model
+	tree, err := hoeffding.New(model, "play", &hoeffding.Config{
+		GracePeriod:     2,
+		SplitConfidence: 0.1,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// Train
+	for _, x := range examples {
+		tree.Train(x)
+	}
+
+	// Predict
+	prediction := tree.PredictCategory(core.MapExample{
+		"outlook":  "rainy",
+		"temp":     "mild",
+		"humidity": "high",
+		"windy":    "false",
+	})
+
+	// Print categories with probabilities
+	fmt.Printf("yes: %.2f\n", prediction.Prob(0))
+	fmt.Printf(" no: %.2f\n", prediction.Prob(1))
+
+	// Output:
+	// yes: 0.40
+	//  no: 0.60
+}
+
+func Example_regression() {
+	model := core.NewModel(
+		core.NewNumericalFeature("hours"),
+		core.NewCategoricalFeature("outlook", []string{"rainy", "overcast", "sunny"}),
+		core.NewCategoricalFeature("temp", []string{"hot", "mild", "cool"}),
+		core.NewCategoricalFeature("humidity", []string{"normal", "high"}),
+		core.NewCategoricalFeature("windy", []string{"true", "false"}),
+	)
+
+	examples := []core.MapExample{
+		{"outlook": "rainy", "temp": "hot", "humidity": "high", "windy": "false", "hours": 25},
+		{"outlook": "rainy", "temp": "hot", "humidity": "high", "windy": "true", "hours": 30},
+		{"outlook": "overcast", "temp": "hot", "humidity": "high", "windy": "false", "hours": 46},
+		{"outlook": "sunny", "temp": "mild", "humidity": "high", "windy": "false", "hours": 45},
+		{"outlook": "sunny", "temp": "cool", "humidity": "normal", "windy": "false", "hours": 52},
+		{"outlook": "sunny", "temp": "cool", "humidity": "normal", "windy": "true", "hours": 23},
+		{"outlook": "overcast", "temp": "cool", "humidity": "normal", "windy": "true", "hours": 43},
+		{"outlook": "rainy", "temp": "mild", "humidity": "high", "windy": "false", "hours": 35},
+		{"outlook": "rainy", "temp": "cool", "humidity": "normal", "windy": "false", "hours": 38},
+		{"outlook": "sunny", "temp": "mild", "humidity": "normal", "windy": "false", "hours": 46},
+		{"outlook": "rainy", "temp": "mild", "humidity": "normal", "windy": "true", "hours": 48},
+		{"outlook": "overcast", "temp": "mild", "humidity": "high", "windy": "true", "hours": 52},
+		{"outlook": "overcast", "temp": "hot", "humidity": "normal", "windy": "false", "hours": 44},
+		{"outlook": "sunny", "temp": "mild", "humidity": "high", "windy": "true", "hours": 30},
+	}
+
+	// Init with a model
+	tree, err := hoeffding.New(model, "hours", &hoeffding.Config{
+		GracePeriod:     2,
+		SplitConfidence: 0.1,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// Train
+	for _, x := range examples {
+		tree.Train(x)
+	}
+
+	// Predict
+	prediction := tree.PredictValue(core.MapExample{
+		"outlook":  "rainy",
+		"temp":     "mild",
+		"humidity": "high",
+		"windy":    "false",
+	})
+
+	// Print mean value with weight
+	fmt.Printf("value: %.2f, weight: %.0f\n", prediction.Number(), prediction.Weight())
+
+	// Output:
+	// value: 42.67, weight: 6
+}
